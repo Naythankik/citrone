abc9d7990bd6cb0a4d7eae95bc4d3710ec99dfc3
Faruq-Hameed <faruqhameed1@gmail.com>
2023-04-19 12:07:01 +0100

    testing user creation endpoint

--- src/utils/joiSchema/signUpSchema.js
+++ src/utils/joiSchema/signUpSchema.js
@@ -6,7 +6,7 @@ function signUpSchema(data) {
     lastName: Joi.string().min(2).max(16).valid().required(),
     phoneNumber: Joi.string().required(),
     email: Joi.string().email().required().lowercase(),
-    password: Joi.string().alphanum().min(6).max(16).required(),
+    password: Joi.string().min(6).max(16).required(),
   });
   return schema.validate(data);
 }
