e5fd57cb9af574031ad51ce03937ef6a030f051f
naythankik <dolapokiki3ird@gmail.com>
2023-04-19 11:38:04 +0100

    WIP

--- src/utils/joiSchema/signUpSchema.js
+++ src/utils/joiSchema/signUpSchema.js
@@ -2,8 +2,8 @@ const Joi = require("joi");
 function signUpSchema(data) {
   //user sign up schema (post & put request)
   const schema = Joi.object({
-    firstName: Joi.string().min(2).max(16).required(),
-    lastName: Joi.string().min(2).max(16).required(),
+    firstName: Joi.string().min(2).max(16).valid().required(),
+    lastName: Joi.string().min(2).max(16).valid().required(),
     phoneNumber: Joi.string().required(),
     email: Joi.string().email().required().lowercase(),
     password: Joi.string().alphanum().min(6).max(16).required(),
