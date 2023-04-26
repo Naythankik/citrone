bf3c5f87081b3cc7673c8d933ca795abd7fff423
Faruq-Hameed <faruqhameed1@gmail.com>
2023-04-26 09:24:26 +0100

    added passport npm and refactor chat controller

--- src/controllers/auth/googleAuth.js
+++ src/controllers/auth/googleAuth.js
@@ -1,41 +1,12 @@
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const passport = require("passport");
 const { User } = require("../../models");
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
+// Google Sign-In API verification middleware
 
-module.exports = (passport) => {
-        passport.use(new GoogleStrategy({
-            clientID: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/google/callback",
-            passReqToCallback : true
-          },
-          async (request, accessToken, refreshToken, profile, done) => {
-            try {
-                let existingUser = await User.findOne({ 'google.id': profile.id });
-                <em>// if user exists return the user</em> 
-                if (existingUser) {
-                  return done(null, existingUser);
-                }
-                <em>// if user does not exist create a new user</em> 
-                console.log('Creating new user...');
-                const newUser = new User({
-                  method: 'google',
-                  google: {
-                    id: profile.id,
-                    name: profile.displayName,
-                    email: profile.emails[0].value
-                  }
-                });
-                await newUser.save();
-                return done(null, newUser);
-            } catch (error) {
-                return done(error, false)
-            }
-          }
-        ));
-    }
+
 // Define Routes
 // Two routes are needed in order to allow users to log in with their Google account. The first route redirects the user to the Google, where they will authenticate:
 
--- src/controllers/chatControllers.js
+++ src/controllers/chatControllers.js
@@ -5,12 +5,9 @@ const { StatusCodes } = require("http-status-codes");
 const { chatSchema } = require("../utils/joiSchema");
 
 const createChat = async (req, res, next) => {
-  /**validate the data sent from the client
-   * the two usersId (sender and receiver) will
-   * be sent together with the text and all chats between the
+  /**validate the data sent from the client 
+   * the validated data include text and the receiverId/ partnerId
    * two users will be returned back to the sender */
-  // later the code will be modified that the userId will be
-  // gotten from the user payload instead from the client
   const { userId } = req.payload;
   try {
     const validation = chatSchema(req.body);
@@ -91,75 +88,6 @@ const getAllChatsOfAUser = async (req, res, next) => {
   res.status(StatusCodes.OK).json({ data: sortedChats });
 };
 
-// const getAllChatsOfAUser = async (req, res, next) => {
-//     const { userId } = req.payload;
-
-//     /**Get all chats where the user is a participant sorted by date*/
-//     const allChats = await Chat.find({
-//       $or: [{ sender: userId }, { receiver: userId }],
-//     }).sort({ createdAt: -1 });
-//     const sortedChats = []; //the finally sorted chats array will be returned to the caller
-//     /**loop through the chats and separate chat he has with different users */
-
-//     for (let i = 0; i < allChats.length; i++) {
-//       let chat = allChats[i]; //a particular chat object from allChats array(array of chat objects)
-//       let { receiver, sender } = chat;
-//       const currentUserId = new mongoose.Types.ObjectId(userId);
-//       const partnerId = (sender === currentUserId) ? receiver : sender;
-//       /**check if the partner and the user chats have been added to the sortedChats already */
-//       if (
-//         sortedChats.some(
-//           (chat) => chat.receiver === partnerId || chat.sender === partnerId
-//         )
-//       )
-//         continue; //skip the loop if the chat with the partner is already in the sortedChats
-
-//       //the chat with the partner here will be added to the chat with the partner
-//       let chatWithPartner = allChats.find(
-//         (chat) => (chat.sender.equals(currentUserId) && chat.receiver.equals(partnerId)) || (chat.sender.equals(partnerId) && chat.receiver.equals(currentUserId))
-//       );
-
-//       if (chatWithPartner !== null) {
-//         sortedChats.push(chatWithPartner);
-//       }
-//     }
-//     res.status(StatusCodes.OK).json({ data: sortedChats });
-//   };
-
-const getAllChatsWithOtherUsers = async (req, res, next) => {
-  const { userId } = req.payload;
-
-  // Find all chats where the user is either the sender or receiver
-  const allChats = await Chat.find({
-    $or: [{ sender: userId }, { receiver: userId }],
-  }).sort({ createdAt: 1 });
-
-  // Group the chats by the other user's ID
-  const chatsByUser = allChats.reduce((accumulator, chat) => {
-    // Get the ID of the other user in the chat
-    const otherUserId =
-      chat.sender.toString() === userId ? chat.receiver : chat.sender;
-
-    // If the other user already has a group of chats, add the current chat to that group
-    if (accumulator[otherUserId]) {
-      accumulator[otherUserId].push(chat);
-    }
-    // If the other user doesn't have a group of chats yet, create a new group with the current chat
-    else {
-      accumulator[otherUserId] = [chat];
-    }
-
-    return accumulator;
-  }, {});
-
-  // Sort each group of chats by date
-  Object.keys(chatsByUser).forEach((userId) => {
-    chatsByUser[userId].sort((a, b) => a.createdAt - b.createdAt);
-  });
-
-  res.status(StatusCodes.OK).json({ data: chatsByUser });
-};
-
 const getUserChatsWithAnotherUser = async (req, res, next) => {
   try {
     const { userId } = req.payload;
@@ -179,7 +107,8 @@ const getUserChatsWithAnotherUser = async (req, res, next) => {
       )
       .map((chatObject) => chatObject._id.toString()); //return only the string chat id of the chats
     if (chatIds.length > 0) {
-      const result = await Chat.updateMany(
+      /**update all the chats sent to the users which are unread to read */
+      await Chat.updateMany(
         { _id: { $in: chatIds } },
         { $set: { read: true } }
       );
