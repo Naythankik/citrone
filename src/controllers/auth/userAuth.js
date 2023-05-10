d74d752cee4f1b0993bee6d1bccfcec5f87eb124
Faruq-Hameed <faruqhameed1@gmail.com>
2023-05-10 18:22:47 +0100

    fixing email re-verification

--- src/controllers/auth/userAuth.js
+++ src/controllers/auth/userAuth.js
@@ -53,8 +53,8 @@ const userLogin = async (req, res, next) => {
 
     //check if the user status is pending
     if (user.status !== "approved") {
-      return res.status(401).send({ message: "user account has not been verified" });
-      // return next(); //we resend a verification token again if the user status is pending 
+      // return res.status(401).send({ message: "user account has not been verified" });
+      return next(); //we resend a verification token again if the user status is pending 
     }
 
     /*check if a user is signed in on the device(the browser) at the moment and logout the user */
